98b0324f5748d743d44a516ae50886dcfa57f610
MatchaChoco010 <MatchaChoco010@users.noreply.github.com>
2022-06-04 03:11:50 +0000

    deploy: f199166ecabbdef6b2a9e32bc179ec2ef222c9d9

--- app-1207ab6ae95126d1.js
+++ app-1207ab6ae95126d1.js
@@ -321,7 +321,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('app-711f44458b96b6cc_bg.wasm', import.meta.url);
+        input = new URL('app-1207ab6ae95126d1_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
